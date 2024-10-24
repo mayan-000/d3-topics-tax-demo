6fa4490b0d5be58ca693a8793f4d293b6d25f57b
Mayank Rana <mayankranax1@gmail.com>
2024-10-24 12:12:36 +0530

    Remove bubbles and show tree only

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import Pack from "./circle.js";
 import Tree from "./tree.js";
 
 const taxonomyUrl =
@@ -12,49 +11,14 @@ const loadTaxonomy = async () => {
 
 const taxonomy = await loadTaxonomy();
 
-const packConfig = {
-  value: (d) => d.size,
-  label: (d, n) => [...d.name.split(/(?=[A-Z][a-z])/g)].join("\n"),
-  title: (d, n) =>
-    `${n
-      .ancestors()
-      .reverse()
-      .map(({ data: d }) => d.name)
-      .join(".")}\n${n.value.toLocaleString("en")}`,
-  link: (d, n) => `#${d.name.replace(/\s/g, "-")}`,
-  width: 1500,
-  height: 1250,
-	linkTarget: "_self",
-};
-
 const treeConfig = {
-  label: (d) => d.name,
-  title: (d, n) =>
-    `${n
-      .ancestors()
-      .reverse()
-      .map((d) => d.data.name)
-      .join(".")}`,
-  link: (d, n) => console.log(d, n),
   width: 2000,
 };
 
 const render = async () => {
-  const hash = window.location.hash
-    ? window.location.hash.slice(1).split("-").join(" ")
-    : "";
-
-  let chart = hash
-    ? await Tree(taxonomy, hash, treeConfig)
-    : await Pack(taxonomy, packConfig);
+  let chart = await Tree(taxonomy, treeConfig)
   const node = document.getElementById("root");
   node.appendChild(chart);
 };
 
 render();
-
-window.onhashchange = () => {
-	const node = document.getElementById("root");
-	node.innerHTML = "";
-	render();
-}
